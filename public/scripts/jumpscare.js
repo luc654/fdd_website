d4ab503e427212d321697e43a71309b7dbcd64a8
luc654 <lucas.dieleman@outlook.com>
2025-11-30 11:34:27 +0100

    Feat: some other scary things

--- public/scripts/jumpscare.js
+++ public/scripts/jumpscare.js
@@ -1,14 +1,39 @@
-const fullPath = '/public/gifs/fnaf-jumpscare.gif'
-var randomWidth = 100000;
+const fullPath = ''
+var randomWidth = 10000;
+
+
+const jumpscareObjects = {
+    "1": {
+        "path": '/public/gifs/skeleton_run.gif',
+        "animated": false,
+        'timer': 1000,
+        'weight': 1
+    },
+    "2": {
+        "path": '/public/gifs/skeleton_run.gif',
+        "animated": true,
+        'timer': 2000,
+        'weight': 3,
+    }
+}
+
 
 async function jumpscare(){
 
-    const element = `<div id="jumpscare" class="w-full h-[1000px] fixed bottom-0 bg-[url(${fullPath})] bg-cover" style="background-position: calc(60%) center;"></div>`;
+    const keys = Object.keys(jumpscareObjects);
+const randomKey = keys[Math.floor(Math.random() * keys.length)];
+const object = jumpscareObjects[randomKey];
+
+
+    console.log(object)
+    const element = `<div id="jumpscare" class="w-full h-[1000px] fixed bottom-0 bg-[url(${object['path']})] bg-cover" style="background-position: calc(60%) center;"></div>`;
     document.querySelector('body').innerHTML += element;
-    await sleep(1000);
-    document.getElementById('jumpscare').remove();
+    await sleep(object['timer']);
+    // document.getElementById('jumpscarebeïnvloedt').remove();
 
     
+
+
 
 clearInterval(timer);
 timer = setInterval(jumpscare, parseInt(Math.random() * randomWidth));
@@ -16,5 +41,4 @@ timer = setInterval(jumpscare, parseInt(Math.random() * randomWidth));
 
 }
 
-// The random range will be from 0 to 3000, or whatever you want !
 var timer = setInterval(jumpscare, parseInt(Math.random() * randomWidth));
