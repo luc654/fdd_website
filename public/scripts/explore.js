9ac213fc4df7adfeaf7576fc70b7655b2ffaa383
luc654 <lucas.dieleman@outlook.com>
2025-12-17 13:37:44 +0100

    FEAT: workign sorting algorithm

--- public/scripts/explore.js
+++ public/scripts/explore.js
@@ -2,24 +2,40 @@ const articleId = 'article';
 
 
 
+
+document.querySelectorAll('#search_button').forEach(element => {
+    element.addEventListener('click', (e) => {
+        document.querySelectorAll('.buttonSelected').forEach(element => {
+            element.classList.remove('buttonSelected');
+        });
+        e.target.classList.add('buttonSelected');
+        filter(e.target.dataset.searchinfo)
+    });
+});
+
+
 // Retrieve from URL function
 document.addEventListener('DOMContentLoaded', () => {
     const params = parseURLParams(window.location.href);
 
-    console.log(params);
-    filter("HTML / CSS");
+    if (params && 'cat' in params) {
+        filter(params.cat);
+    }
+
+    filter('html/css');
+
 });
 
 
 
 
 
-// Filter takes a category type shit or something man i fucking hate writing comments just fucking look at the code dumbass
+
+// Filter takes a category, determines what subcategories belong to it and hides all those who dont contain those subcategories.
 async function filter(category){
     const JSON = await fetchJSON();
     const list = returnOnlySubcategoriesFromJSON(JSON, category);
-    console.log(list);
-    iterateThroughAllArticlesAndRemoveWithoutGivenList();
+    iterateThroughAllArticlesAndRemoveWithoutGivenList(list);
 
 
 }
@@ -43,11 +59,23 @@ function returnOnlySubcategoriesFromJSON(json, category){
 }
 
 // dont talk shit about 'uhh function too long' this is a helper function and is only needed once and gives all the explanation.
-function iterateThroughAllArticlesAndRemoveWithoutGivenList(){
+function iterateThroughAllArticlesAndRemoveWithoutGivenList(categoriesSubcat){
     document.querySelectorAll(`#${articleId}`).forEach((article) => {
         if(article.dataset.categories){
             const categories = article.dataset.categories.split(",");
-            console.log(categories);
+            let flag = false;
+            categories.forEach((subCat)  =>{
+                categoriesSubcat.forEach((acceptableElement) =>{
+                    if(subCat == acceptableElement){
+                        flag = true;
+                        article.classList.remove('hidden');
+                        return;
+                    }
+                });
+                if(!flag){
+                    article.classList.add('hidden');        
+                }
+            });
         } else {
             article.classList.add('hidden');
         }
