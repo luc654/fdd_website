2a4bf7af6995d01b78e2f6353a4165fb87a5196c
luc654 <lucas.dieleman@outlook.com>
2025-12-17 12:21:13 +0100

    fix: carousel gap issues

--- public/scripts/carousel.js
+++ public/scripts/carousel.js
@@ -1,7 +1,9 @@
 import Splide from '@splidejs/splide';
 import '@splidejs/splide/dist/css/splide.min.css';
 
-console.log("a");
 document.addEventListener('DOMContentLoaded', () => {
-  new Splide('.splide').mount();
+  new Splide('.splide', {
+     type   : 'loop',
+    gap: '1rem',
+  }).mount();
 });
