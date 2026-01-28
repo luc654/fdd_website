537f4c6dfb4a6fafa1a14eb06a915cbd1461edd1
Luc654 <lucas.dieleman@outlook.com>
2026-01-28 16:53:48 +0100

    RF: bigger default text

--- public/scripts/blog.js
+++ public/scripts/blog.js
@@ -1,13 +1,20 @@
 const target = 'text-section';
-const sizes = ['text-base', 'text-lg', 'text-xl']
+const sizes = ['text-lg', 'text-xl', 'text-2xl'];
 let index = 0;
+
+const elements = document.querySelectorAll(`#${target}`);
+
 document.getElementById('enlargeText').addEventListener('mousedown', () => {
-    document.querySelectorAll(`#${target}`).forEach(elem => {
+    const nextIndex = (index + 1) % sizes.length;
+
+    elements.forEach(elem => {
         elem.classList.remove(sizes[index]);
-        elem.classList.add(sizes[index + 1]);
+        elem.classList.add(sizes[nextIndex]);
     });
-    index++
-    if(index > sizes.length - 1){
-        index = 0;
-    }
+
+    index = nextIndex;
+});
+
+elements.forEach(elem => {
+    elem.classList.add(sizes[index]);
 });
